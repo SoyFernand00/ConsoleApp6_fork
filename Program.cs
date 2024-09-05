66cd1bc192463858cf95825828c028b5a311cf94
Fabri-Programmer <fabrizzziopks@hotmail.com>
2024-09-05 09:35:53 -0500

    Primer commit

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*Console.WriteLine("Hello, World!");
+for (int fernando = 1; fernando < 101; fernando++)
+{
+    Console.WriteLine(fernando);
+}
+
+float lolo = 1.9f;
+Console.WriteLine(lolo);
+double lolo1 = 1 - 9;
+Console.WriteLine(lolo1); 
+*/
+
+/*var Myarray = new string[] { "nicolas ", "fernando", "fabri", "andre" };
+Console.WriteLine(Myarray[3]);
+
+foreach (var jony in Myarray)
+{
+    Console.WriteLine(jony);
+} */
+
+/*for (int descendente = 50; descendente >= 20; descendente--)
+{
+    Console.WriteLine(descendente);
+} */
+using System.Diagnostics.CodeAnalysis;
+
+/*int suma = 0;
+for (int chico = 0; chico <= 100; chico = chico + 5)
+{
+    Console.WriteLine(chico);
+    suma = suma + chico;
+}
+Console.WriteLine($"\nLa suma sera de todo {suma}");
+*/
+/*int r, total=0;
+int suma = 0;
+int promedio = 0;
+for (int i = 1; i <=4; i++)
+{
+    Console.Write("ingresa numero = ");
+    r = int.Parse(Console.ReadLine());
+    suma = suma + r;
+    
+    
+}
+Console.WriteLine("\nesta es la suma de todo los numeros " + suma);
+Console.WriteLine("\nesta es la suma de todo los numeros " +(total = suma / 4));
+*/
+double num1 = 0;
+Console.Write("Cuantos numeros quieres promediar = ");
+num1 = double.Parse(Console.ReadLine());
+double suma = 0;
+
+for (int num = 0; num < num1; num++)
+{
+    double calif;
+    Console.Write("Dame las calificaciones = ");
+    calif = double.Parse(Console.ReadLine());
+    suma = suma + calif;
+}
+double promedio = 0;
+
+Console.WriteLine("esta es la suma " + suma);
+Console.WriteLine("este es el promedio " + promedio);
+
